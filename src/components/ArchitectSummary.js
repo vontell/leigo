30cb7fb1e6bd22f2b061397e0d1f70a12dc24ee6
Himitsuan <sidrad@gmail.com>
2018-04-14 16:20:12 -0400

    Made ArchitectSummary component unstyled

---
+++ src/components/ArchitectSummary.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class ArchitectSummary extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className = "title" > {this.prop.title} </h1>
+        <p className = "description">  {this.prop.description} </p>
+        <p className = "budget">  {this.prop.budget} </p>
+        <p className = "deadline"> {this.prop.deadline} days</p>
+      </div>
+    );
+  }
+}
