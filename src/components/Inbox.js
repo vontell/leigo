498232e38434070ea107189dc9818dd6e0287d04
tpatel1781 <tpatel1781@gmail.com>
2018-04-14 15:28:27 -0400

    Added components for Inbox and InboxItem. Unstyled.

---
+++ src/components/Inbox.js
@@ -0,0 +1,12 @@
+class Inbox extends React.Component {
+	render() {
+		return (
+			<div className="inbox">
+				<h1>Inbox</h1>
+				<div name="inbox-list">
+					{React.Children.map(this.props.inboxList, i => <InboxItem name={i.name} description={i.description} />)}
+				</div>
+			</div>
+		);
+	}
+}
---
+++ src/components/InboxItem.js
@@ -0,0 +1,10 @@
+class InboxItem extends React.Component {
+	render() {
+		return (
+			<div className="inbox-item">
+				<h1>{this.props.name}</h1>
+				<p>{this.props.description}</p>
+			</div>
+		);
+	}
+}
