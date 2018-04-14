3d3d1732067209384cc1757b7661c1483449ca6f
Neil Patel <neilp980@yahoo.com>
2018-04-14 18:45:41 -0400

    Added text input box to chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -8,6 +8,10 @@ export default class Chat extends React.Component {
 				<div name="chat-list">
 					{React.Children.map(this.props.chatlist, i => <MessageBox userimg={i.userimg} message={i.message} username={i.username} timestamp={i.timestamp}/>)}
 				</div>
+                <form>
+                    <div><textarea name="message" rows="10" cols="30">Enter Chat Here</textarea></div>
+                    <div><input type="submit" value="Send"></input></div>
+                </form>
 			</div>
 		);
 	}
