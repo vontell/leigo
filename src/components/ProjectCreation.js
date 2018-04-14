6771857af7f2d12603aebf1209e8c759888eb4ca
Neil Patel <neilp980@yahoo.com>
2018-04-14 17:03:03 -0400

    Some changes

--- src/components/ProjectCreation.js
+++ src/components/ProjectCreation.js
@@ -10,20 +10,20 @@ class ProjectCreation extends React.Component {
             <div className = "title"> <h2> Leigo </h2> </div>
             <div className = "description">
                 <h2> Description: </h2>
-                <textarea>
+                <textarea name = "descriptionBox">
                     Enter your project description here
                 </textarea>
             </div>
             <div className = "budget">
                 <h2> Budget: </h2>
-                <input type="number" value="Under"> </input>
+                <input type="number" value="Under" name="budgetTextBox"> </input>
             </div>
             <div className = "deadline">
                 <h2> Deadline: </h2>
-                <input type="date" value="MM/DD/YYYY"> </input>
+                <input type="date" value="MM/DD/YYYY" name = "deadlineTextBox"> </input>
             </div>
             <div className = "architectureButton">
-                 <input type="checkbox" id="architecture" name="architecture" value="Architecture"> </input>
+                 <input type="checkbox" name="architectureCheckbox" value="Architecture"> </input>
                  <label for="architecture">Create Your own Architecture</label>
             </div>
             <div>
