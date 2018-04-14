cc1ef8fad125952f60c94effe6e9b05ab32cf7c1
Neil Patel <neilp980@yahoo.com>
2018-04-14 16:22:23 -0400

    Chagnes one thing

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,7 +4,7 @@ export default class TaskList extends React.Component {
     render() {
 		return (
 			<div className="tasks">
-				<h1>Inbox</h1>
+				<h1>Tasks</h1>
 				<div name="task-list">
 					{React.Children.map(this.props.tasklist, i => <TaskCard title={i.title} tech={i.tech} timeScale={i.timeScale}/>)}
 				</div>
