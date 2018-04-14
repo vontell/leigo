98a2f17f1cd476126aadd2e9e2582425f038b46e
tpatel1781 <tpatel1781@gmail.com>
2018-04-14 16:03:54 -0400

    Added conditional colors for accepted status of user.

--- src/components/UserPreview.js
+++ src/components/UserPreview.js
@@ -1,7 +1,7 @@
 class UserPreview extends React.Components {
 	render() {
 		return(
-			<div className="user-preview">
+			<div className="user-preview" style={{backgroundColor: this.props.accepted ? "#00000": "#00000"}}>
 				<img src={this.props.image} />
 				<div name="name-rating">
 					<h1>{this.props.name}</h1>
