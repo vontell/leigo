7f3d8c7eeb2aea74e0b0ffc49d1dd49180f1f626
Himitsuan <sidrad@gmail.com>
2018-04-14 15:32:16 -0400

    Made TaskCard component Unstyled

---
+++ src/components/TaskCard.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import React from 'react';
+
+export default class TaskCard extends React.Component {
+  render() {
+    const divStyle = {
+      margin: '40px',
+      border: '5px solid blue'
+    };
+    return (
+     <div className= "divStyle"/>
+    );
+  }
+}
