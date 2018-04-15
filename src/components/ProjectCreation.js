d701f6e2554faf0a41d17ad92496f5c6cee71092
Himitsuan <sidrad@gmail.com>
2018-04-14 20:57:18 -0400

    What the hell did Sid do with this CSS

--- src/components/ProjectCreation.js
+++ src/components/ProjectCreation.js
@@ -10,20 +10,28 @@ export default class ProjectCreation extends React.Component {
             divTop : {
                 width : '100%',
                 height : '33.333%',
-                background : '#253237'
+                background : '#253237',
+                position: 'fixed',
+                top: '0px',
+                left: '0px'
             },
             
             divBackground : {
+                color: '#253237',
                 backgroundColor : '#F2F2F4',
-                textAlign : 'center'
+                textAlign : 'center',
+                fontFamily: 'Open Sans',
+                marginTop: '200px'
             },
 
             divCenter : {
-                position : 'fixed',
-                bottom : '0px',
+                position : 'relative',
                 width : '60%',
-                height : '80%',
-                textAlign : 'center'
+                height : '500px',
+                textAlign : 'left',
+                backgroundColor: '#FFFFFF',
+                margin: 'auto auto',
+                top: '100px'
             }
 
         }
@@ -34,17 +42,17 @@ export default class ProjectCreation extends React.Component {
                     <form>
                         <div className = "title"> <h2> Leigo </h2> </div>
                         <div className = "description">
-                            <h2> Description: </h2>
+                            <h2> Description </h2>
                             <textarea name = "descriptionBox">
                                 Enter your project description here
                             </textarea>
                         </div>
                         <div className = "budget">
-                            <h2> Budget: </h2>
+                            <h2> Budget </h2>
                             <input type="number" name="budgetTextBox"/> 
                         </div>
                         <div className = "deadline">
-                            <h2> Deadline: </h2>
+                            <h2> Deadline </h2>
                             <input type="date" value="MM/DD/YYYY" name = "deadlineTextBox"/> 
                         </div>
                         <div className = "architectureButton">
