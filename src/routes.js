eb08aeba242f03e67574af4f27f06b1ee903995e
vontell <vontell@mit.edu>
2018-04-14 23:19:35 -0400

    Route changes

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,19 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router';
 
-
+import Layout from './components/Layout';
 import NotFoundPage from './components/NotFoundPage';
 import ArchitecturePage from './components/ArchitecturePage';
 import ProjectCreation from './components/ProjectCreation';
 
 const routes = (
-  <Route path="/" component={ArchitecturePage}>
-    
+
+  <Route path="/" component={Layout}>
+    <IndexRoute component={ProjectCreation}/>
+    <Route path="/architecture" component={ArchitecturePage}/>
+    <Route path="*" component={NotFoundPage}/>
   </Route>
+
 );
 
 export default routes;
