57802d6c2cf32960adb907711879a0c29a099f96
Himitsuan <sidrad@gmail.com>
2018-04-14 21:33:59 -0400

    Changes to ProjectCreation style

--- src/components/ProjectCreation.js
+++ src/components/ProjectCreation.js
@@ -7,52 +7,72 @@ export default class ProjectCreation extends React.Component {
     render() {
 
         const style = {
-            divTop : {
-                width : '100%',
-                height : '33.333%',
-                background : '#253237',
-                position: 'fixed',
-                top: '0px',
-                left: '0px'
-            },
-            
+
             divBackground : {
                 color: '#253237',
                 backgroundColor : '#F2F2F4',
                 textAlign : 'center',
                 fontFamily: 'Open Sans',
-                marginTop: '200px'
+                height: "120%"
             },
 
+            divTop : {
+                width : '100%',
+                height : '300px',
+                background : '#253237',
+            },
+            
             divCenter : {
-                position : 'relative',
                 width : '60%',
                 height : '500px',
                 textAlign : 'left',
                 backgroundColor: '#FFFFFF',
                 margin: 'auto auto',
-                top: '100px'
+                marginTop: "-100px",
+                borderRadius: "4px",
+                padding: "32px",
+                marginBottom: "150px"
+            },
+            header: {
+                fontSize: "32px",
+            },
+            subheader: {
+                fontSize: "18px",
+                color: "grey",
+                paddingTop: "8px",
+                paddingBottom: "4px",
+            },
+            largeInput: {
+                width: "100%",
+                height: "150px",
+                padding: "12px 12px",
+                boxSizing: "border-box",
+                border: "1px solid #f8f8f8",
+                borderRadius: "8px",
+                backgroundColor: "#f8f8f8",
+                resize: "none",
+                fontSize: "14px"
             }
 
         }
 		return(
+
+            
             <div style = {style.divBackground}>
                 <div style = {style.divTop}/>
-                <div style = {style.divCenter}>
+                <div style = {style.divCenter} class="z-depth-4">
                     <form>
-                        <div className = "title"> <h2> Leigo </h2> </div>
+                        <div> <h2 style={style.header}> Leigo </h2> </div>
                         <div className = "description">
-                            <h2> Description </h2>
-                            <textarea name = "descriptionBox">
-                                Enter your project description here
-                            </textarea>
+                            <h2 style={style.subheader}> Description </h2>
+                            <textarea name = "descriptionBox" style={style.largeInput}/>
                         </div>
                         <div className = "budget">
-                            <h2> Budget </h2>
+                            <h2 style={style.subheader}> Budget </h2>
                             <input type="number" name="budgetTextBox"/> 
                         </div>
                         <div className = "deadline">
-                            <h2> Deadline </h2>
+                            <h2 style={style.subheader}> Deadline </h2>
                             <input type="date" value="MM/DD/YYYY" name = "deadlineTextBox"/> 
                         </div>
                         <div className = "architectureButton">
